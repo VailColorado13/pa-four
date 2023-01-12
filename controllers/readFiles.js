aeb21fedfb7c61b5265226b09c81e7a6a51bb596
Matt Vail <mattvailco@gmail.com>
2023-01-12 11:13:02 -0700

    css connected

--- controllers/readFiles.js
+++ controllers/readFiles.js
@@ -9,26 +9,22 @@ module.exports = {
         fileZero = files[0]
         const rawText = await mammoth.extractRawText({path: './uploads' + '/' + fileZero})
         return [files, rawText.value]
-      },
+    },
       
     getJobNumber: async function (rawText) {
       const regex = /(?:JOB #:).*(?:ISCI)/gm
       const match = rawText.match(regex) 
       const jobNumber = match[0].substring(6, match[0].length -4).trim()
-      if (jobNumber) {
-        return jobNumber
-      }
-      else {
-        return 'INSERT JOB NUMBER'
-      }
+      if (jobNumber) return jobNumber
+      else return 'INSERT JOB NUMBER'
     }, 
 
     getClientName: async function (rawText) {
       const regex = /(?:CLIENT:).*(?:CAMPAIGN)/gm
       const match = rawText.match(regex) 
       const clientName = match[0].substring(7, match[0].length -8).trim()
-      console.log('clientName' , clientName)
-      return clientName
+      if (clientName) return clientName
+      else return 'INSERT CLIENT NAME'
     }
 
 
--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require('express')
 const ejs = require('ejs')
 const mainRoutes = require('./routes/mainRoutes')
+const path = require('path')
+
 
 const app = express()
 app.set('view engine' , 'ejs')
+app.use(express.static(__dirname + '/public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
 
 app.use('/', mainRoutes)
+
+
 
 app.listen(1337, () => {
     console.log(`server running on 1337`)
